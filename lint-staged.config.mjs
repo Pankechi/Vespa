325585e91ac7e840b6ceb221837fce9ca2172fc1
alekseyamozov <spacemarce@gmail.com>
2025-11-18 14:34:59 +0300

    add: linters were fixed

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,5 @@
 export default {
   '*.{js, jsx, ts, tsx}': ['prettier --write', 'eslint --fix'],
-  '*.vue': ['prettier --write', 'eslint --fix'],
+  '*.vue': ['stylelint --fix', 'prettier --write', 'eslint --fix'],
+  '*.scss': ['stylelint --fix', 'prettier --write'],
 };
